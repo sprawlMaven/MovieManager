10db82f669ea927ad7d0870eb30fab600b1fc39f
Maura Goodwin <maurasg@icloud.com>
2019-09-12 14:13:21 -0700

    Initial Commit

---
+++ UdacityMovieManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UdacityMovieManager
+//
+//  Created by Maura Goodwin on 9/12/19.
+//  Copyright © 2019 Information Architects LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
